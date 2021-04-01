b4d5b9e1e65b906a23ac72bab2ec16954f5fed5d
YORDAN RUSEV Mobilyaine <y.rusev04@gmail.com>
2021-04-02 01:13:49 +0300

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            while (!command.Equals("End"))
+            {
+                Console.WriteLine("In while loop");
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("Out of while");
+        }
+    }
+}
